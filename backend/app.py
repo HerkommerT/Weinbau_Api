cc838dffc5067b3528a31bad630d1cbde36b6d98
HerkommerT <t.herkommer1@gmx.net>
2024-09-07 20:18:18 +0200

    Test Verbindung

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Dies erlaubt alle Ursprünge. Du kannst dies anpassen, um spezifische Ursprünge zuzulassen.
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
+
+if __name__ == "__main__":
+    app.run()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-// src/App.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Button } from 'primereact/button';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 
@@ -15,14 +16,42 @@ const Home = () => (
     </div>
 );
 
-const Menu = () => (
-    <div>
-        <h1>Menu Page</h1>
-        <Link to="/">
-            <Button label="Go to Home" />
-        </Link>
-    </div>
-);
+const Menu = () => {
+    const [incomes, setIncomes] = useState([]);
+
+    // useEffect wird verwendet, um die Daten beim Laden der Komponente zu holen
+    useEffect(() => {
+        // GET-Anfrage an die Flask-API mit fetch, um die Einkommensdaten zu erhalten
+        fetch('http://127.0.0.1:5000/incomes')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Die erhaltenen Daten werden im Zustand gespeichert
+                setIncomes(data);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }, []); // Der leere Array sorgt dafür, dass die Anfrage nur einmal bei der Montage ausgeführt wird
+
+    return (
+        <div>
+            <h1>Menu Page</h1>
+            {/* Anzeige der Einkommensdaten in einer DataTable */}
+            <DataTable value={incomes}>
+                <Column field="description" header="Description"></Column>
+                <Column field="amount" header="Amount"></Column>
+            </DataTable>
+            <Link to="/">
+                <Button label="Go to Home" />
+            </Link>
+        </div>
+    );
+};
 
 function App() {
     return (

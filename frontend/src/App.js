efc9542c2a79b747932d79357e5ee73cbcbf8292
HerkommerT <t.herkommer1@gmx.net>
2024-09-18 19:52:19 +0200

    Add Toolbar with dialog in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Dialog } from 'primereact/dialog';
+import { Toolbar } from 'primereact/toolbar';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 
 // Beispiel-Komponenten
 const Home = () => (
@@ -18,10 +22,18 @@ const Home = () => (
 
 const Menu = () => {
     const [wein, setWeingueter] = useState([]);
+    const [dialogVisible, setDialogVisible] = useState(false);
+    const [newWein, setNewWein] = useState({
+        name: '',
+        beschr: '',
+        preis: '',
+        weingut_id: '',
+        typ_id: '',
+        art_id: ''
+    });
 
     // useEffect wird verwendet, um die Daten beim Laden der Komponente zu holen
     useEffect(() => {
-        // GET-Anfrage an die Flask-API mit fetch, um die Weingut-Daten zu erhalten
         fetch('http://127.0.0.1:5000/wein')
             .then(response => {
                 if (!response.ok) {
@@ -30,27 +42,155 @@ const Menu = () => {
                 return response.json();
             })
             .then(data => {
-                // Die erhaltenen Daten werden im Zustand gespeichert
                 setWeingueter(data);
             })
             .catch(error => {
                 console.error('There was a problem with the fetch operation:', error);
             });
-    }, []); // Der leere Array sorgt dafür, dass die Anfrage nur einmal bei der Montage ausgeführt wird
+    }, []);
+
+    const handleDialogClose = () => {
+        setDialogVisible(false);
+    };
+
+    const handleDialogOpen = () => {
+        setDialogVisible(true);
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewWein({
+            ...newWein,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://127.0.0.1:5000/wein', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newWein)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setWeingueter([...wein, data]);
+                handleDialogClose();
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    };
+
+    const MyToolbar = () => {
+        const [dialogVisible, setDialogVisible] = useState(false);
+    
+        const handleDialogOpen = () => {
+            setDialogVisible(true);
+        };
+    
+        const handleDialogClose = () => {
+            setDialogVisible(false);
+        };
+    
+        const start = (
+            <React.Fragment>
+                <Button label="Add Wein" icon="pi pi-plus" onClick={handleDialogOpen} />
+            </React.Fragment>
+        );
+    
+        return (
+        <div>
+        <Toolbar start={start} />
+                
+        {dialogVisible && (
+            <Dialog header="Add New Wein" visible={dialogVisible} style={{ width: '50vw' }} footer={<Button label="Close" icon="pi pi-times" onClick={handleDialogClose} />} onHide={handleDialogClose}>
+                <form onSubmit={handleSubmit}>
+                    <div className="p-field">
+                        <label htmlFor="name">Name</label>
+                        <input id="name" name="name" value={newWein.name} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="beschr">Beschreibung</label>
+                        <input id="beschr" name="beschr" value={newWein.beschr} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="preis">Preis</label>
+                        <input id="preis" name="preis" value={newWein.preis} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="weingut_id">Weingut ID</label>
+                        <input id="weingut_id" name="weingut_id" value={newWein.weingut_id} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="typ_id">Typ ID</label>
+                        <input id="typ_id" name="typ_id" value={newWein.typ_id} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="art_id">Art ID</label>
+                        <input id="art_id" name="art_id" value={newWein.art_id} onChange={handleInputChange} />
+                    </div>
+                    <Button type="submit" label="Submit" />
+                </form>
+            </Dialog>
+                )}
+        </div>
+        );
+    };
 
     return (
         <div>
             <h1>Menu Page</h1>
-            {/* Anzeige der Daten in einer DataTable */}
+            <MyToolbar />
+            <Button label="Add Wein" icon="pi pi-plus" onClick={handleDialogOpen} />
+
             <DataTable value={wein}>
-                <Column field="wein_id" header="Wein ID"></Column>
-                <Column field="name" header="">Name</Column>
-                <Column field="beschr" header="Beschreibung"></Column>
-                <Column field="preis" header="Preis"></Column>
-                <Column field="weingut_id" header="Weingut ID"></Column>
-                <Column field="typ_id" header="Typ ID"></Column>
-                <Column field="art_id" header="Art ID"></Column>
+                <Column field="wein_id" header="Wein ID" />
+                <Column field="name" header="Name" />
+                <Column field="beschr" header="Beschreibung" />
+                <Column field="preis" header="Preis" />
+                <Column field="weingut_id" header="Weingut ID" />
+                <Column field="typ_id" header="Typ ID" />
+                <Column field="art_id" header="Art ID" />
             </DataTable>
+
+            <Dialog header="Add New Wein" visible={dialogVisible} style={{ width: '50vw' }} footer={<Button label="Close" icon="pi pi-times" onClick={handleDialogClose} />} onHide={handleDialogClose}>
+                <form onSubmit={handleSubmit}>
+                    <div className="p-field">
+                        <label htmlFor="name">Name</label>
+                        <input id="name" name="name" value={newWein.name} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="beschr">Beschreibung</label>
+                        <input id="beschr" name="beschr" value={newWein.beschr} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="preis">Preis</label>
+                        <input id="preis" name="preis" value={newWein.preis} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="weingut_id">Weingut ID</label>
+                        <input id="weingut_id" name="weingut_id" value={newWein.weingut_id} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="typ_id">Typ ID</label>
+                        <input id="typ_id" name="typ_id" value={newWein.typ_id} onChange={handleInputChange} />
+                    </div>
+                    <div className="p-field">
+                        <label htmlFor="art_id">Art ID</label>
+                        <input id="art_id" name="art_id" value={newWein.art_id} onChange={handleInputChange} />
+                    </div>
+                    <Button type="submit" label="Submit" />
+                </form>
+            </Dialog>
+
             <Link to="/">
                 <Button label="Go to Home" />
             </Link>

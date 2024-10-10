3f0b40977a05e1bee59e77da356322066b28cfb9
HerkommerT <t.herkommer1@gmx.net>
2024-10-10 11:13:15 +0200

    change at model_fin.py: adding own primary key

--- backend/models/fin/model_fin.py
+++ backend/models/fin/model_fin.py
@@ -3,10 +3,13 @@ from extensions import db
 class Fin(db.Model):
     __tablename__ = 'absatz'
     
-    # Zusammengesetzter Primärschlüssel aus Fremdschlüsseln
-    wein_id = db.Column(db.Integer, db.ForeignKey('wein.wein_id'), primary_key=True)
-    typ_id = db.Column(db.Integer, db.ForeignKey('typ.typ_id'), primary_key=True)
-    art_id = db.Column(db.Integer, db.ForeignKey('art.art_id'), primary_key=True)
+    # Auto increment, not null, primary key
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    
+    # Fremdschlüssel
+    wein_id = db.Column(db.Integer, db.ForeignKey('wein.wein_id'), nullable=False)
+    typ_id = db.Column(db.Integer, db.ForeignKey('typ.typ_id'), nullable=False)
+    art_id = db.Column(db.Integer, db.ForeignKey('art.art_id'), nullable=False)
     
     # Weitere Attribute
     stückzahl = db.Column(db.Integer, nullable=False)
@@ -17,4 +20,4 @@ class Fin(db.Model):
     art = db.relationship('Art', backref=db.backref('fins', lazy=True))
 
     def __repr__(self):
-        return f'<Fin {self.wein_id}-{self.typ_id}-{self.art_id}>'
+        return f'<Fin {self.id}: {self.wein_id}-{self.typ_id}-{self.art_id}>'
